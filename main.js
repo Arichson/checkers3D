4a102831099e76f8c957e5b7e876644928cdf111
Arichson <arichsonh@gmail.com>
2021-06-24 16:49:17 -0700

    completed 3d board

--- main.js
+++ main.js
@@ -12,81 +12,62 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(50);
-camera.position.setX(0);
+camera.position.set(0,50,50);
 
 renderer.render(scene, camera);
 
-const verticesOfCube = [
-  -1, -1, -1,    1, -1, -1,    1,  1, -1,    -1,  1, -1,
-  -1, -1,  1,    1, -1,  1,    1,  1,  1,    -1,  1,  1,
-];
-const indicesOfFaces = [
-  2, 1, 0,    0, 3, 2,
-  0, 4, 7,    7, 3, 0,
-  0, 1, 5,    5, 4, 0,
-  1, 2, 6,    6, 5, 1,
-  2, 3, 7,    7, 6, 2,
-  4, 5, 6,    6, 7, 4,
-];
-const radius = 7;  
-const detail = 5;  
-const circle = new THREE.PolyhedronGeometry(
-  verticesOfCube, indicesOfFaces, radius, detail);
-circle.addEventListener("click", () => {console.log("hi")})
+const redSquares = [];
+const blackSquares = [];
+const geometry = new THREE.BoxGeometry(1.8, .5, 1.8);
+const gameBoard = new THREE.BoxGeometry(17, .3, 17);
+const redSquareMaterial = new THREE.MeshBasicMaterial({color: 0x870900});
+const blackSquareMaterial = new THREE.MeshBasicMaterial({color: 0x575757});
+const yellow = new THREE.MeshBasicMaterial({color: 0xb2b500});
+const circMat = new THREE.MeshBasicMaterial({color: 0xb2b500, wireframe: true })
 
-const geometry = new THREE.TorusGeometry(10,1,10,12);
-const geometrys = new THREE.TorusGeometry(5,1,10,12);
-const material = new THREE.MeshStandardMaterial({color: 0xffffff
-  // , wireframe: true
-});
-const color3 = new THREE.Color("rgba(255, 255, 0, .1)");
-const circMaterial = new THREE.MeshBasicMaterial({ color: color3 });
-const circ = new THREE.Mesh(circle, circMaterial)
-const stuff = new THREE.Mesh(geometrys,material)
-const stuff2 = new THREE.Mesh(geometrys,material)
-const stuff3 = new THREE.Mesh(geometrys,material)
-const stuff4 = new THREE.Mesh(geometrys,material)
-const torus = new THREE.Mesh(geometry, material);
-const torus2 = new THREE.Mesh(geometry, material);
-const torus3 = new THREE.Mesh(geometry, material);
-const torus4 = new THREE.Mesh(geometry, material);
-circ.position.set(5,50,50)
+const center = new THREE.IcosahedronGeometry(1, 2);
 
+for( let j = 0; j < 8; j++){
 
-
-scene.add(circ)
-scene.add(torus);
-scene.add(torus2);
-scene.add(torus3);
-scene.add(torus4);
-scene.add(stuff)
-scene.add(stuff2)
-scene.add(stuff3)
-scene.add(stuff4)
-// const newShape = new THREE.Mesh(geometry, material)
-// scene.add(newShape)
+  for( let i = 0; i < 4; i++ ){
+    // console.log(i%2 === 0)
+    const newRed = new THREE.Mesh(geometry, redSquareMaterial);
+    const newBlack = new THREE.Mesh(geometry, blackSquareMaterial);
+    // newRed.position.set(4*i +2 ,0,2*j)
+    // newBlack.position.set(4*i ,0,2*j)
+    j%2 === 1? newRed.position.set(-4*i + 5 ,0,-2*j+7) : newRed.position.set(-4*i +7,0,-2*j+7)
+    j%2 === 0? newBlack.position.set(-4*i +5 ,0,-2*j+7) : newBlack.position.set(-4*i +7,0,-2*j+7)
+    // j%2 === 1? newRed.position.set(4*i +2 ,0,2*j) : newRed.position.set(4*i ,0,2*j)
+    // j%2 === 0? newBlack.position.set(4*i +2 ,0,2*j) : newBlack.position.set(4*i ,0,2*j)
+    redSquares.push(newRed)
+    blackSquares.push(newBlack)
+  }
+}
+const theCenter = new THREE.Mesh(center, circMat)
+theCenter.position.set(0,0,0)
+scene.add(theCenter)
+const theBoard = new THREE.Mesh(gameBoard, yellow)
+theBoard.position.set(0,0,0)
+scene.add(theBoard)
+// scene.add(blackSquares[0]);
+// scene.add(redSquares[0]);
+blackSquares.forEach( (square, i) => {
+  scene.add(square)
+  scene.add(redSquares[i])
+})
 renderer.render(scene, camera)
+
+
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(50,50,50)
 scene.add((pointLight))
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
 function animate () {
   requestAnimationFrame(animate)
-  // stuff.rotation.x += .01;
-  stuff.rotation.y += .02;
-  stuff2.rotation.y -= .02;
-  stuff3.rotation.x += .02;
-  stuff4.rotation.x -= .02;
-  // stuff.rotation.z += .05;
-  // torus.rotation.y += .04;
-  // torus.rotation.z += .08;
-  torus.rotation.x += .02;
-  torus2.rotation.y -= .02;
-  torus3.rotation.x -= .02;
-  torus4.rotation.y += .02
+  // smallSquare.rotation.x += .02;
 
 
   controls.update();
