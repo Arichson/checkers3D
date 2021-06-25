b93b64bbcaa42911edee784be63acd080468d93c
Arichson <arichsonh@gmail.com>
2021-06-24 18:42:53 -0700

    fixed up some excess codes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,62 +13,62 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(0,50,50);
 
-renderer.render(scene, camera);
+camera.position.set(0, 20,0);
 
-const redSquares = [];
-const blackSquares = [];
-const geometry = new THREE.BoxGeometry(1.8, .5, 1.8);
-const gameBoard = new THREE.BoxGeometry(17, .3, 17);
-const redSquareMaterial = new THREE.MeshBasicMaterial({color: 0x870900});
-const blackSquareMaterial = new THREE.MeshBasicMaterial({color: 0x575757});
+///////////// Game Board ( not including the small squares )
+const gameBoard = new THREE.BoxGeometry(17, .2, 17);
 const yellow = new THREE.MeshBasicMaterial({color: 0xb2b500});
-const circMat = new THREE.MeshBasicMaterial({color: 0xb2b500, wireframe: true })
-
-const center = new THREE.IcosahedronGeometry(1, 2);
-
-for( let j = 0; j < 8; j++){
-
-  for( let i = 0; i < 4; i++ ){
-    // console.log(i%2 === 0)
-    const newRed = new THREE.Mesh(geometry, redSquareMaterial);
-    const newBlack = new THREE.Mesh(geometry, blackSquareMaterial);
-    // newRed.position.set(4*i +2 ,0,2*j)
-    // newBlack.position.set(4*i ,0,2*j)
-    j%2 === 1? newRed.position.set(-4*i + 5 ,0,-2*j+7) : newRed.position.set(-4*i +7,0,-2*j+7)
-    j%2 === 0? newBlack.position.set(-4*i +5 ,0,-2*j+7) : newBlack.position.set(-4*i +7,0,-2*j+7)
-    // j%2 === 1? newRed.position.set(4*i +2 ,0,2*j) : newRed.position.set(4*i ,0,2*j)
-    // j%2 === 0? newBlack.position.set(4*i +2 ,0,2*j) : newBlack.position.set(4*i ,0,2*j)
-    redSquares.push(newRed)
-    blackSquares.push(newBlack)
-  }
-}
-const theCenter = new THREE.Mesh(center, circMat)
-theCenter.position.set(0,0,0)
-scene.add(theCenter)
 const theBoard = new THREE.Mesh(gameBoard, yellow)
 theBoard.position.set(0,0,0)
 scene.add(theBoard)
-// scene.add(blackSquares[0]);
-// scene.add(redSquares[0]);
-blackSquares.forEach( (square, i) => {
-  scene.add(square)
-  scene.add(redSquares[i])
-})
-renderer.render(scene, camera)
+
+/////////////Squares on the board
+const smallSquares = new THREE.BoxGeometry(1.8, .5, 1.8);
+const redSquareMaterial = new THREE.MeshBasicMaterial({color: 0x870900});
+const blackSquareMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
+
+///////// Positions the squares on the board and places it on the board
+for( let j = 0; j < 8; j++){
+  for( let i = 0; i < 4; i++ ){
+    const newRed = new THREE.Mesh(smallSquares, redSquareMaterial);
+    const newBlack = new THREE.Mesh(smallSquares, blackSquareMaterial);
+    j%2 === 1? newRed.position.set(-4*i + 5 ,0,-2*j+7) : newRed.position.set(-4*i +7,0,-2*j+7)
+    j%2 === 0? newBlack.position.set(-4*i +5 ,0,-2*j+7) : newBlack.position.set(-4*i +7,0,-2*j+7)
+    scene.add(newBlack, newRed)
+  }
+}
+
+////// This puts the peices on the board
+// const peices = new THREE.CylinderGeometry(.6, .6, 1, 30);
+// const circMat = new THREE.MeshBasicMaterial({color: 0xffffff})
+// const thePeices = new THREE.Mesh(peices, circMat)
+// thePeices.position.set( 0, 0 , 0)
+// scene.add(thePeices)
 
 
+
+
+////// This just shows the center of the board
+const center = new THREE.CylinderGeometry(.6, .6, 1, 30);
+const circMat = new THREE.MeshBasicMaterial({color: 0xffffff})
+const theCenter = new THREE.Mesh(center, circMat)
+theCenter.position.set( 0, 0 , 0)
+scene.add(theCenter)
+
+
+//////// adds a light source 
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(50,50,50)
 scene.add((pointLight))
 
+//////// allows orbit of hte board and zoom in/out
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
+///// recursive function to keep updating the camera and render the images
 function animate () {
   requestAnimationFrame(animate)
-  // smallSquare.rotation.x += .02;
 
 
   controls.update();
@@ -75,8 +76,3 @@ function animate () {
   renderer.render(scene, camera)
 }
 animate();
-
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
